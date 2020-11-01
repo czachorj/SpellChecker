0ac75680373b5ddb92146edcffff3c8d6d958d2f
sengheta <sarah.engheta@gmail.com>
2020-11-01 16:42:00 -0500

    adding files and creating method for putting text file into it's own array

--- src/WordRecommender.java
+++ src/WordRecommender.java
@@ -6,9 +6,9 @@ public class WordRecommender {
 
 	// read from dictionary file
 
-	String[] readFile(){
+	String[] readDict(){
 		File dictionary = new File ("engDictionary.txt");
-		ArrayList<String> engDict = new ArrayList<>();
+		ArrayList<String> engDict = new ArrayList<>(); //creating an arraylist for the dictionary words
 		try {
 			Scanner scnr = new Scanner(dictionary);
 			while (scnr.hasNext()) {
@@ -23,7 +23,26 @@ public class WordRecommender {
 
 	}
 
+	
+	//read test file to spellcheck
+	String[] readFile(){
+		File dictionary = new File ("filetotest.txt");
+		ArrayList<String> testFile = new ArrayList<>(); //creating an arraylist for the paragraph to be tested 
+		try {
+			Scanner scnr = new Scanner(dictionary);
+			while (scnr.hasNext()) {
+				testFile.add(scnr.next());
+			}
 
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 
+	}
+
+	String []
+	
 
 }
