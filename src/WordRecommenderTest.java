71a10400ec0025710f0a165573ee165ee08cca1f
sengheta <sarah.engheta@gmail.com>
2020-11-04 14:57:12 -0500

    created new wordrecommenderTest file

--- src/WordRecommenderTest.java
+++ src/WordRecommenderTest.java
@@ -1,6 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
 
-public class WordRecommenderTest {
-	
-	// unit tests here
-	
+import org.junit.jupiter.api.Test;
+
+class WordRecommenderTest {
+
+	@Test
+	void testGetSimilarity() {
+		
+		WordRecommender test1 = new WordRecommender("filename", "otherfile");
+		String word1 = "apple";
+		String word2 = "arcade";
+		assertEquals(test1.getSimilarity(word1, word2),1);
+	}
 }
+//
+//	@Test
+//	void testGetCommonPercent() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testGetWordSuggestions() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testPrettyPrint() {
+//		fail("Not yet implemented");
+//	}
+//
+//}
